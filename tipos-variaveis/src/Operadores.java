d5ccd514f8866a3c30daf99f24837903fceed211
nelson <sinhofreitas@gmail.com>
2025-04-05 19:28:39 -0300

    aprendizados com operadores

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,38 @@
+public class Operadores {
+    public static void main(String[] args) throws Exception {
+        
+        boolean condicao1=true;
+
+        boolean condicao2=true;
+
+        if (condicao1 && condicao2){
+            System.out.println("as duas condiçoes sao verdadeiras");
+        }
+        if (condicao1 || condicao2){
+            System.out.println("uma das  condiçoes é verdadeiras");
+        }
+
+            System.out.println("fim");
+           /*  int numero1 = 1;
+            int numero2 = 2;
+
+            boolean simNao = numero1 == numero2;
+
+            if (numero1 < numero2){
+                System.out.println("a nossa condica é verdadeira");
+            }
+
+            System.out.println("numeroUm é igual a numeroDois? " + simNao);
+
+            simNao = numero1 != numero2;
+
+            System.out.println("numeroUm é igual a numeroDois? " + simNao);
+            simNao = numero1 < numero2;
+
+            System.out.println("numeroUm é menor que numeroDois? " + simNao);
+*/ 
+
+
+        
+    } 
+}
--- tipos-variaveis/src/TiposVariaves.java
+++ tipos-variaveis/src/TiposVariaves.java
@@ -1,5 +1,7 @@
 public class TiposVariaves {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+      String nomeCompleto = "Nelson" + "JAVA";
+
+      System.out.println(nomeCompleto);
     }
 }
